945841b3efb47587244e2b5f7c83cb1e95adaa86
NguyenVanDucCS22 <duw3210@gmail.com>
2024-11-05 23:03:54 +0700

    a

--- nvd/dao.py
+++ nvd/dao.py
@@ -2,4 +2,24 @@ import json
 
 def load():
     with open('data/categories.json', encoding= 'utf-8') as f:
-        return json.load(f)
+        categories=json.load(f)
+
+    return categories
+    
+
+def LoadProducts(q=None, query=None):
+    with open('data/products.json', encoding= 'utf-8') as f:
+        products=json.load(f)
+        if q:
+            products = [p for p in products if q.lower() in p["name"].lower()]
+        if query:
+            products = [p for p in products if  p["category_id"].__eq__(int(query))]
+
+        return products
+
+def LoadProduct_Detail(id):
+    with open('data/products.json', encoding= 'utf-8') as f:
+        products=json.load(f)
+        for p in products:
+            if p["id"]==int(id):
+                return p
--- nvd/index.py
+++ nvd/index.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 import dao
 
 app = Flask(__name__)
+
+
 @app.route('/')
 
 def index():
+    q=request.args.get('q')
+
+    # trangchu=request.args.get('trangchu')
+    query = request.args.get('query')
+
+    # Trả về danh sách sản phẩm tìm kiếm theo từ khóa, query và trang hiện tại
     categories = dao.load()
-    return render_template('index.html',categories=categories)
+    
+    products = dao.LoadProducts(q=q,query= query)
+    return render_template('index.html',categories=categories, products=products)
+
+@app.route('/query/<int:id>')
+def truyxuat(id):
+    categories = dao.load()
+    product = dao.LoadProduct_Detail(id)
+    return render_template('detailProduct.html', product=product, categories=categories) 
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
